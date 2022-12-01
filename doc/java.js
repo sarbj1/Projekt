23f14e58ae1e008c19cbe6ed3ece57c11d06afc0
jonas.sarbach <sarbj1@bfh.ch>
2022-12-01 09:50:36 +0100

    Mobile Behavior

--- doc/java.js
+++ doc/java.js
@@ -23,3 +23,26 @@ appearOptions);
 articles.forEach((article) => {
   appearOnScroll.observe(article);
 });
+
+/*
+Reference
+https://sahithyandev.github.io/sv443-joke-api-js-wrapper/
+*/
+
+console.log(Object.values(JokeAPI));
+
+// JokeAPI.getJokes().then(r => console.log(r.body))
+JokeAPI.getJokes({
+  jokeType: "single",
+})
+  .then((r) => r.json())
+  .then((data) => {
+    updateUI(data);
+  });
+
+// To update the joke on the UI
+function updateUI(jokeData) {
+  const $ = (id) => document.getElementById(id);
+
+  $("joke--text").innerHTML = jokeData.joke;
+}
